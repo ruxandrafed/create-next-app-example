418329b96181d34675af6efd297104ebf2428adc
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2019-03-31 11:08:53 -0700

    add fix to make app run locally

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const withCss = require('@zeit/next-css')
 
+if (typeof require !== "undefined") {
+  require.extensions[".less"] = () => {};
+  require.extensions[".css"] = (file) => {};
+}
+
 module.exports = withCss({
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
