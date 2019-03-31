b0e18fdfd8609368f51f427a83e980fd2d169b16
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2019-03-31 11:07:56 -0700

    initial commit with css setup

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withCss = require('@zeit/next-css')
+
+module.exports = withCss({
+  webpack: config => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: 'empty'
+    }
+
+    return config
+  }
+})
